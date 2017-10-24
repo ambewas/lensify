f92830b1f1041e80b0d12ce6b729efd9fcadce52
Kevin Decock <kevin.decock@district01.be>
2017-10-24 10:02:45 +0200

    fix deeper keys. They should use lensPath instead of lensProp

--- src/index.js
+++ src/index.js
@@ -1,23 +1,27 @@
-import { lensProp } from 'ramda';
+import { lensProp, lensPath } from 'ramda';
 
-const lensify = state => {
+const lensify = (state, path) => {
   const keys = Object.keys(state);
 
   return keys.reduce((acc, key) => {
     if (typeof state[key] === "object") {
       const deeperKey = state[key];
+      const deeperPath = `${key}.${Object.keys(deeperKey)[0]}`;
 
       return {
         ...acc,
-        [key]: lensify(deeperKey),
+        [key]: lensify(deeperKey, deeperPath),
       };
     }
 
+    const lensFunction = path ? lensPath(path.split(".")) : lensProp(key);
+
     return {
       ...acc,
-      [key]: lensProp(key),
+      [key]: lensFunction,
     };
   }, {});
 };
+
 
 export default lensify;
